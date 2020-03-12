203871d900cc78ede14e5f4afc490b4ae78b3cdd
viradhanus <59219626+viradhanus@users.noreply.github.com>
2020-03-12 20:15:16 +0530

    added

---
+++ Day 05/ex7.s
@@ -0,0 +1,52 @@
+@ ARM Assembly - exercise 7 
+@ Group Number :
+
+	.text 	@ instruction memory
+
+	
+@ Write YOUR CODE HERE	
+
+@ ---------------------	
+Fibonacci:
+
+
+
+
+
+
+
+
+
+
+
+@ ---------------------
+	
+	.global main
+main:
+	@ stack handling, will discuss later
+	@ push (store) lr to the stack
+	sub sp, sp, #4
+	str lr, [sp, #0]
+
+	mov r4, #8 	@the value n
+
+	@ calling the Fibonacci function
+	mov r0, r4 	@the arg1 load
+	bl Fibonacci
+	mov r5,r0
+	
+
+	@ load aguments and print
+	ldr r0, =format
+	mov r1, r4
+	mov r2, r5
+	bl printf
+
+	@ stack handling (pop lr from the stack) and return
+	ldr lr, [sp, #0]
+	add sp, sp, #4
+	mov pc, lr
+
+	.data	@ data memory
+format: .asciz "F_%d is %d\n"
+
---
+++ Day 06/lab3_2.s
@@ -0,0 +1,56 @@
+@ ARM Assembly - lab 3.2 
+@ Group Number :
+
+	.text 	@ instruction memory
+
+	
+@ Write YOUR CODE HERE	
+
+@ ---------------------	
+gcd:
+
+
+
+
+
+
+
+
+
+
+
+@ ---------------------	
+
+	.global main
+main:
+	@ stack handling, will discuss later
+	@ push (store) lr to the stack
+	sub sp, sp, #4
+	str lr, [sp, #0]
+
+	mov r4, #64 	@the value a
+	mov r5, #24 	@the value b
+	
+
+	@ calling the mypow function
+	mov r0, r4 	@the arg1 load
+	mov r1, r5 	@the arg2 load
+	bl gcd
+	mov r6,r0
+	
+
+	@ load aguments and print
+	ldr r0, =format
+	mov r1, r4
+	mov r2, r5
+	mov r3, r6
+	bl printf
+
+	@ stack handling (pop lr from the stack) and return
+	ldr lr, [sp, #0]
+	add sp, sp, #4
+	mov pc, lr
+
+	.data	@ data memory
+format: .asciz "gcd(%d,%d) = %d\n"
+
